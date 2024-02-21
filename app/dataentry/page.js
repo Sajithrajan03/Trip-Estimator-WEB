8be8f20e9ce6ce80cf650229801fa053b34e1a4e
Sajithrajan03 <saisajith03@gmail.com>
2024-02-21 23:38:31 +0530

    First

---
+++ app/dataentry/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaDatabase } from "react-icons/fa";
+import DataEntry from "@/app/dataentry/DataEntry"
+const page = () => {
+  return (
+    <div className='bg-[#9bd3f3] w-full min-h-screen'>
+        <DataEntry/>
+    </div>
+  )
+}
+
+export default page
