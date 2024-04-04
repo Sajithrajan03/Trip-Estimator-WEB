7cc6eb8e5928bba4508b25418de7ac37e3041cdc
Saravanak Karthick <sara.karth158@gmail.com>
2024-04-04 19:44:57 +0530

    modified the contact us page

--- app/_Component/ContactUs.jsx
+++ app/_Component/ContactUs.jsx
@@ -1,3 +1,4 @@
+/*
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarker, faPhone, faEnvelope, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
@@ -155,6 +156,171 @@ function Contact() {
 
 )}
 
+    </section>
+  );
+}
+
+export default Contact;
+*/
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarker, faPhone, faEnvelope, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+  const [submissionStatus, setSubmissionStatus] = useState('');
+  const [successMessageVisible, setSuccessMessageVisible] = useState(false); // State to control the visibility of success message
+
+  const [nameFocused, setNameFocused] = useState(false);
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [messageFocused, setMessageFocused] = useState(false);
+  const { name, email, message } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    setSubmissionStatus('success');
+  
+    // Perform validation
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+      alert('Please fill in all fields');
+      return; // Prevent form submission if fields are not filled
+    }
+  
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert('Please enter a valid email address');
+      return; 
+    }
+  
+    // Your form submission logic here
+    console.log('Form submitted:', formData);
+    setSubmitted(true);
+    setSuccessMessageVisible(true); // Show success message
+
+    // Reset form data after success message is displayed
+    setFormData({
+      name: '',
+      email: '',
+      message: ''
+    });
+  };
+
+  useEffect(() => {
+    // If submissionStatus is 'success', set a timeout to change it to null after 3 seconds
+    if (successMessageVisible) {
+      const timeout = setTimeout(() => {
+        setSuccessMessageVisible(false); // Hide success message after 3 seconds
+      }, 3000);
+
+      // Clear the timeout when the component unmounts or successMessageVisible changes
+      return () => clearTimeout(timeout);
+    }
+  }, [successMessageVisible]);
+
+  return (
+    <section id="contact" className="bg-blue-900 bg-opacity-10 py-5 mt-11">
+      <h1 className="section-header text-white text-center text-5xl font-semibold uppercase mb-8">Contact Us</h1>
+      <div className="contact-wrapper flex flex-col md:flex-row justify-between mx-auto max-w-3xl">
+        <form id="contact-form" className="form-horizontal max-w-md md:mr-8 mx-auto" onSubmit={onSubmit} role="form">
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control text-black border border-blue-600 rounded p-2 mb-4 w-full transition-transform duration-300 ease-in-out " 
+              id="name" 
+              placeholder="NAME" 
+              name="name" 
+              value={name} 
+              onChange={onChange} 
+              onFocus={() => setNameFocused(true)} 
+              onBlur={() => setNameFocused(false)} 
+              required 
+              style={{ transform: nameFocused ? "scale(1.02)" : "scale(1)" }}
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="email" 
+              className="form-control text-black border border-gray-600 rounded p-2 mb-4 w-full transition-transform duration-300 ease-in-out" 
+              id="email" 
+              placeholder="EMAIL" 
+              name="email" 
+              value={email} 
+              onChange={onChange} 
+              onFocus={() => setEmailFocused(true)} 
+              onBlur={() => setEmailFocused(false)} 
+              required 
+              style={{ transform: emailFocused ? "scale(1.02)" : "scale(1)" }}
+            />
+          </div>
+          <div className="form-group">
+            <textarea 
+              className="form-control text-black border border-gray-600 rounded p-2 mb-4 w-full transition-transform duration-300 ease-in-out" 
+              rows="10" 
+              placeholder="MESSAGE" 
+              name="message" 
+              value={message} 
+              onChange={onChange} 
+              onFocus={() => setMessageFocused(true)} 
+              onBlur={() => setMessageFocused(false)} 
+              required 
+              style={{ transform: messageFocused ? "scale(1.02)" : "scale(1)" }}
+            ></textarea>
+          </div>
+          <button 
+            className="btn btn-primary send-button w-full bg-transparent hover:bg-blue-800 text-white hover:text-black border border-white rounded p-2 transition duration-200" 
+            type="submit"
+          >
+            <div className="alt-send-button">
+              <FontAwesomeIcon icon={faPaperPlane} size="s" className="text-white mr-2 bg-transparent hover:bg-blue" />
+              <span className="send-text">SEND</span>
+            </div>
+          </button>
+        </form>
+        <div className={`direct-contact-container max-w-md mx-auto md:ml-8 mt-8 md:mt-0 ${submitted ? 'ml-auto' : ''}`}>
+          <ul className="contact-list">
+            <li className="list-item">
+              <FontAwesomeIcon icon={faMapMarker} size="xl" className="text-white mr-6 mt-10" />
+              <span className="contact-icon text-white mr-2 place">SAP LABS, Bengaluru</span>
+            </li>
+            <li className="list-item">
+              <FontAwesomeIcon icon={faPhone} size="xl" className="text-white mr-6 mt-10" />
+              <span className="contact-text phone mr"><a href="tel:1-212-555-5555" title="Give me a call" className="text-white">+496227747474</a></span>
+            </li>
+            <li className="list-item">
+              <FontAwesomeIcon icon={faEnvelope} size="xl" className="text-white mr-6 mt-10" />
+              <span className="contact-text gmail"><a href="mailto:#" title="Send me an email" className="text-white">sapnetwork@sap.com</a></span>
+            </li>
+          </ul>
+          <hr className="border-white mt-8" />
+          <ul className="social-media-list flex justify-center items-center mt-4">
+            <li><a href="https://github.com/Sajithrajan03/Trip-Estimator-WEB" target="_blank" className="contact-icon text-white mr-4 text-xl"><FontAwesomeIcon icon={faGithub} /></a></li>
+            <li><a href="https://twitter.com/" target="_blank" className="contact-icon text-white mr-4 text-xl"><FontAwesomeIcon icon={faTwitter} /></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" className="contact-icon text-white mr-4 text-xl"><FontAwesomeIcon icon={faInstagram} /></a></li>
+          </ul>
+          <hr className="border-white mt-3" />
+          <div className="copyright text-center text-gray-500">&copy; ALL OF THE RIGHTS RESERVED</div>
+        </div>
+      </div>
+      {successMessageVisible && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+          <div className="bg-black bg-opacity-25 absolute inset-0 backdrop-filter backdrop-blur-lg"></div> 
+          <div className="bg-transparent rounded-lg p-6 shadow-none relative z-10">
+            <p className="text-3xl text-center text-blue-500 font-bold bg-white p-4 rounded-xl">
+              Thank you for reaching out. We'll be in contact shortly!
+            </p>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
