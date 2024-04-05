e72c74072d1846832485f6dd523520d18e1218ea
Sajithrajan03 <saisajith03@gmail.com>
2024-04-05 13:51:43 +0530

    navbar done

--- app/_Component/Navbar.jsx
+++ app/_Component/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useRef} from 'react';
 import { Toolbar } from 'primereact/toolbar';
 import { Avatar } from 'primereact/avatar';
 import 'primeicons/primeicons.css';
@@ -8,14 +8,16 @@ import ProfileCard from './ProfileCard';
 import secureLocalStorage from 'react-secure-storage';
 import Link from 'next/link';
 import { Menu } from 'primereact/menu';
-
+import 'primeicons/primeicons.css';
+import { useRouter } from 'next/navigation';
+import { comma } from 'postcss/lib/list';
 export default function CustomDemo() {
     const[ userName,setUserName]=useState("")
     useEffect(() => {
       setUserName(secureLocalStorage.getItem("userName"));
     }, []);
      
-  
+    const router =useRouter();
     const startContent = (
         <React.Fragment>
             <div className='cursor-pointer flex group '>
@@ -31,45 +33,89 @@ export default function CustomDemo() {
             </div>
         </React.Fragment>
     );
+    const menuRight = useRef(null);
+    const menu = [
+        {
+            label: 'Options',
+            items: [
+                {
+                    label: 'Apply for Trip',
+                    icon: 'pi pi-plus',
+                    command: () => router.push('/applicant')
+                },
+                {
+                    label: 'View Application',
+                    icon: 'pi  pi-th-large'
+
+                },
+                {
+                    label: 'View Profile',
+                    icon: 'pi pi-user',
+                    command: () => router.push('/profile')
+
+                }
+            ]
+        },
+        {
+            label: 'Help',
+            items: [
+                {
+                    label: 'Contact Us',
+                    icon: 'pi pi-phone',
+                    command: () => router.push('/contactus')
+                },
+                {
+                    label: 'FAQ',
+                    icon: 'pi pi-question-circle',
+                    command: () => router.push('/faq')
+                }
+            ]
+        }
+    ];
 
     const centerContent = (
         <div className="flex justify-between flex-wrap align-items-center gap-8 lg:gap-17 md:gap-12  ">
+            
+            <Menu model={menu} popup ref={menuRight} id="popup_menu_right" popupAlignment="right" 
+            pt={{
+                submenuHeader: { className: 'text-cyan-500  font-bold text-[20px] p-1 flex justify-center rounded-lg mx-2 mb-1' },
+                menu: { className: 'text-blue-900  bg-gray-300 ' },
+                menuitem: { className: 'font-bold text-[15px] flex justify-start text-white w-full' },
+                root: { className : "font-bold bg-gray-300 w-fit" },
+                icon:{className:"font-bold text-black text-[20px] mr-5"},
+                label : {className:"font-bold text-black text-[18px] "}
+            }}/>
             <Link href="/applicant">
-            <div className="p-link inline-flex hover:scale-125 justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
+            <div className="p-link inline-flex hover:scale-110 justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
                 <i className="pi pi-home text-2xl  bg-blue-800 text-gray-200 border-2 border-black p-2 rounded-md ml-4"></i>
             </div>
             </Link>
-            <Link href="/profile">
-            <div className="p-link inline-flex justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
-                <i className="pi pi-user text-2xl hover:scale-125 bg-blue-800 text-gray-200 border-2 border-black p-2 rounded-md ml-1"></i>
+             
+            
+            <div onClick={(event) => menuRight.current.toggle(event)} aria-controls="popup_menu_right" aria-haspopup
+            className="p-link inline-flex hover:scale-110 justify-content-center align-items-center  text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
+            <div className='bg-blue-800 border-2 p-1 border-black  rounded-md ml-1 flex justify-center items-center'>
+                <i className="pi pi-bars text-2xl  text-gray-200 mr-1"></i>
+                <i className='pi pi-angle-double-down text-black text-[16px]'></i>
             </div>
-            </Link>
-            <Link href="/contactus">
-            <div className="p-link inline-flex hover:scale-125 justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
-            <i className="pi pi-phone text-2xl bg-blue-800 text-gray-200 border-2 border-black p-2 rounded-md ml-1"></i>
             </div>
-            </Link>
-
-            <Link href="/faq">
-            <div className="p-link inline-flex hover:scale-125 justify-content-center align-items-center text-white h-3rem w-3rem border-circle hover:bg-white-alpha-10 transition-all transition-duration-200">
-            <i className="pi pi-question-circle text-2xl bg-blue-800 text-gray-200 border-2 border-black p-2 rounded-md ml-1"></i>
-            </div>
-            </Link>
              
         </div>
     );
+    
 
     const endContent = (
         <React.Fragment>
             <div className="flex align-items-center items-center gap-2">
                 <ProfileCard email={"sajith"}/>
-                <span className="font-bold text-blue-800 text-[20px]">{userName}</span>
+                <span className="font-bold text-blue-800 text-[20px]">{userName.toUpperCase()}</span>
             </div>
         </React.Fragment>
     );
 
     return (
         <div className="card">
+            
             <Toolbar start={startContent} center={centerContent} end={endContent} className='lg:w-[950px] xl:w-[1000px] mx-auto mt-4 ' pt={{
         root: { style: { background: 'bg-primary', borderRadius: '24px' } }
     }} />
