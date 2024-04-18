35100f9dc6c83d9f2ea686807d4c57fe1779d943
Sajithrajan03 <azureuser@TripEstimator.plivcwxjwjquhdbrzuxmktakdh.bx.internal.cloudapp.net>
2024-04-18 21:02:31 +0000

    added

--- app/_Component/_util/constants.js
+++ app/_Component/_util/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://172.208.67.125:3000/api"
+const BASE_URL = "http://localhost:3000/api"
 export const LOGIN_URL = BASE_URL+"/login"
 export const ENTER_TRIP_DETAILS_URL = BASE_URL+"/enterTripDetails"
 export const GET_DASHBOARD_DETAILS_URL = BASE_URL+"/getDashboardDetails"
